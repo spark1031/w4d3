e5361301b9894d9f979177306fc7e748705c1f44
Sujin Park <sujinpark1031@gmail.com>
2018-11-14 18:04:59 -0800

    Add #destroy to SessionsController, add CSRF to all forms

--- ninety_nine_cats_ii/app/controllers/application_controller.rb
+++ ninety_nine_cats_ii/app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def login_user!(user)
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def logout_user!
+    session[:session_token] = nil
+    if current_user
+      current_user.reset_session_token!
+    end
+    @current_user = nil
+  end
 end
--- ninety_nine_cats_ii/app/controllers/sessions_controller.rb
+++ ninety_nine_cats_ii/app/controllers/sessions_controller.rb
@@ -4,5 +4,21 @@ class SessionsController < ApplicationController
     render :new
   end
 
+  def create
+    user = User.find_by_credentials(params[:user][:user_name], params[:user][:password])
+    if user
+      login_user!(user)
+      redirect_to cats_url
+    else
+      flash.now[:errors] = ["Invalid username and/or password"]
+      render :new
+    end
+  end
+
+  def destroy
+    logout_user!
+    redirect_to new_session_url
+  end
+
 
 end
