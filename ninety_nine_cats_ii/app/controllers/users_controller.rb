e71c5298c77f8e47d40897be0c78584ede2398c4
Sujin Park <sujinpark1031@gmail.com>
2018-11-14 16:27:54 -0800

    Add UsersController with new and create actions along with new.html.erb in views under users directory

---
+++ ninety_nine_cats_ii/app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  # before_action :require_no_user! #what does this do?? it was in solutions
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      login_user!(@user)
+      redirect_to cats_url
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:user_name, :password)
+  end
+end
